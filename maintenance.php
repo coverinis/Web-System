7c250aef4f749a4d351a5e8dec872e901e9a4ce1
Justin <xhergz@gmail.com>
2017-04-24 14:21:04 -0400

    Added employeeData script to add/insert employee information

--- maintenance.php
+++ maintenance.php
@@ -261,9 +261,9 @@
 		</div>
 		<p class='responseMessage'><?php echo $response; ?></p>
 		<div id="EmpInfo" class="tabcontent">
-			<form id='employeeForm' action='' method='post'>
+			<form id='employeeForm' action='/scripts/employeeData.php' method='post'>
 				<h3 class='customFormLabel'>Select Employee</h3>
-				<select class='customFormInput' id='employeeSelectEmp'>
+				<select class='customFormInput' id='employeeSelectEmp' name='employee_id'>
 					<?php echo GenerateEmployeeList($securityLevel, true); ?>
 				</select>
 				<input type='hidden' value='<?php echo EmpForm; ?>' name='submit_page_type'>
---
+++ scripts/employeeData.php
@@ -0,0 +1,96 @@
+<?php 
+
+//Get the post information from the page
+//employee id
+if(isset($_POST["employee_id"])){
+	if(!empty($_POST["employee_id"])){
+		$employeeID = $_POST["employee_id"];
+	}
+	else{
+		$employeeID = "";
+	}
+}
+else{
+	$employeeID = "";
+}
+
+//Error check if employee ID is not sent
+
+//fname
+if(isset($_POST["fname"])){
+	if(!empty($_POST["fname"])){
+		$firstName = $_POST["fname"];
+	}
+	else{
+		$firstName = "";
+	}
+}
+else{
+	$firstName = "";
+}
+
+//lname
+if(isset($_POST["lname"])){
+	if(!empty($_POST["lname"])){
+		$lastName = $_POST["lname"];
+	}
+	else{
+		$lastName = "";
+	}
+}
+else{
+	$lastName = "";
+}
+
+//sin
+if(isset($_POST["sin"])){
+	if(!empty($_POST["sin"])){
+		$sin = $_POST["sin"];
+	}
+	else{
+		$sin = "";
+	}
+}
+else{
+	$sin = "";
+}
+
+//dob
+if(isset($_POST["dob"])){
+	if(!empty($_POST["dob"])){
+		$dob = $_POST["dob"];
+	}
+	else{
+		$dob = "";
+	}
+}
+else{
+	$dob = "";
+}
+
+//Call bobbys function
+
+//Set up post paramters to go back
+$pageType = $_POST[“submit_page_type”];
+$response = "Success.";
+	
+
+//Test Echo
+echo $employeeID . "<br>";
+echo $firstName . "<br>";
+echo $lastName . "<br>";
+echo $sin . "<br>";
+echo $dob . "<br>";
+	
+	
+?>
+
+<form id="myForm" action="../maintenance.php" method="post">
+    <input type="hidden" name="emp_id" value="<?php echo $employeeID ?>">
+	<input type="hidden" name="page_type" value="<?php echo $pageType ?>">
+	<input type="hidden" name="response" value="<?php echo $response ?>">
+</form>
+ 
+<script type="text/javascript">
+    document.getElementById('myForm').submit();
+</script>
